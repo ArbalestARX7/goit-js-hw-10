58dd26b3b5c95f0941e4d055e1008af96cd5ca88
Ikaruga666 <113863223+ArbalestARX7@users.noreply.github.com>
2023-04-06 19:46:50 +0300

    ДЗ 0.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+function errorNotify() {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
 
 const DEBOUNCE_DELAY = 300;
+const inputField = document.querySelector('#search-box');
+const listOfcountries = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+inputField.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+  let counrtyName = e.target.value.trim();
+  fetchCountries(counrtyName)
+    .then(result => countryMagic(result))
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function countryMagic(countries) {
+  clearMarkup();
+  if (countries.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (countries.length > 1 && countries.length <= 9) {
+    markupListMaker(countries);
+  } else if (countries.length === 1) {
+    markupCountryInfo(countries);
+  }
+}
+
+function markupListMaker(countries) {
+  const markupList = countries
+    .map(
+      ({ name, flags }) =>
+        `<li class="list-item"><img src="${flags.svg}" alt="${flags.alt}" width="40" class="list-img" >${name.official}</li>`
+    )
+    .join('');
+  listOfcountries.insertAdjacentHTML('beforeend', markupList);
+}
+
+function markupCountryInfo(countries) {
+  const markupList = countries
+    .map(
+      ({ name, flags, languages, population, capital }) =>
+        `<img class="info-img" src="${flags.svg}" alt="${
+          flags.alt
+        }" width ="40"/>
+      <h1 class="info-name">${name.official}</h1>
+      <p class="desc"><span class="value">capital:</span>${capital}</p>
+      <p class="desc"><span class="value">population:</span>${population}</p>
+      <p class="desc"><span class="value">languages:</span>${Object.values(
+        languages
+      )}</p>`
+    )
+    .join('');
+  countryInfo.insertAdjacentHTML('beforeend', markupList);
+}
+
+function clearMarkup() {
+  countryInfo.innerHTML = '';
+  listOfcountries.innerHTML = '';
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,10 @@
+import { Notify } from 'notiflix';
+
+export function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(result => result.json())
+    .then(data => data)
+    .catch(error => console.log(error));
+}
