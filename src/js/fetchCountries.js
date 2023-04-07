98ab052774317da271b0eb27f43560f248b8fc01
Ikaruga666 <113863223+ArbalestARX7@users.noreply.github.com>
2023-04-07 20:07:02 +0300

    ДЗ 0.3

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,15 @@
-import { Notify } from 'notiflix';
+const URL = `https://restcountries.com/v3.1/`;
 
 export function fetchCountries(name) {
   return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    `${URL}name/${name}?fields=name,capital,population,flags,languages`
   )
-    .then(result => result.json())
+    .then(result => {
+      if (!result.ok) {
+        throw new Error(result.status);
+      }
+      return result.json();
+    })
     .then(data => data)
     .catch(error => console.log(error));
 }
